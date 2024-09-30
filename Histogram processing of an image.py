0f8af1d5ae5dd9db7c7a8ce91e0c1a4b07955e4f
JerowinGeo <147139744+JerowinGeo@users.noreply.github.com>
2024-09-30 15:33:26 +0530

    Update Histogram processing of an image.py

--- Histogram processing of an image.py
+++ Histogram processing of an image.py
@@ -6,10 +6,10 @@
 
 import cv2
 import matplotlib.pyplot as plt
-gray_image = cv2.imread("gray image of flower.jpg")
-color_image = cv2.imread()
-cv2.imshow("Gray Image",gray_image)
-
+gray_image = cv2.imread('img1.jpg')
+color_image = cv2.imread('img2.jpg')
+cv2.imshow("Gray image",gray_image)
+cv2.imshow("color image",color_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -21,12 +21,13 @@ import matplotlib.pyplot as plt
 grayscale_image=cv2.imread("gray image of flower.jpg")
 colourscale_image=cv2.imread("color image of flower.jpg")
 hist=cv2.calcHist(grayscale_image,[0],None,[255],[0,255])
-hist1=cv2.calcHist()
+hist1=cv2.calcHist([color_img],[1],None,[256],[0,256])
 plt.figure()
+plt.imshow(gray_img)
 plt.title("Histogram")
-plt.xlabel("")
+plt.xlabel("grayscale value")
 plt.ylabel("pixel count")
-plt.stem()
+plt.stem(hist)
 plt.show()
 
 
@@ -36,11 +37,12 @@ plt.show()
 
 
 import cv2
-import matplotlib.pyplot as plt 
-gi=cv2.imread("gray image of flower.jpg",0)
-colorscale=cv2.imread("color image of flower.jpg")
-g=cv2.resize(gi,(500,400))
-equ=cv2.equalizeHist(gi)
+gray_img = cv2.imread('B&W.jpg',0)
+gray_img = cv2.resize(gray_img,(300,200))
+cv2.imshow('Grey Scale Image',gray_img)
+equ = cv2.equalizeHist(gray_img)
+cv2.imshow("Equalized Image",equ)
+cv2.waitKey(0)
 
 
 
